3a63f1073f6787885f2c44152073814b6db3b1af
NdumisoMagunga <magunga55@gmail.com>
2023-11-12 19:41:47 +0200

    working on the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router,Route, Routes } from 
+'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <h1>focus</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' exact />
+      </Routes>
+    </Router>
+    
+    </>
   );
 }
 
